22f4fe6757c322be1db380399501ae48dd448626
SantGT5 <gianspf@gmail.com>
2023-01-21 11:11:54 +0100

    vitest

--- server/src/routers/invalidRouter/__tests__/invalidRouter.spec.ts
+++ server/src/routers/invalidRouter/__tests__/invalidRouter.spec.ts
@@ -1,15 +1,19 @@
 const request = require("supertest")
+import { describe, it, expect } from "vitest"
 
 const BASE_URL = "https://jsm3vu1p49.execute-api.eu-west-1.amazonaws.com"
 
 describe("invalidRouter", () => {
-  test("invalidRouter response with code 404", async () => {
-    const { statusCode, body } = await request(BASE_URL).get("/")
+  it("should response with code 404", async () => {
+    try {
+      const { statusCode, body } = await request(BASE_URL).get("/")
+      const { ok, error } = body
 
-    const { ok, error } = body
-
-    expect(statusCode).toBe(404)
-    expect(ok).toBeFalsy()
-    expect(error).toBe("Not Found")
+      expect(statusCode).toBe(404)
+      expect(ok).toBeFalsy()
+      expect(error).toBe("Not Found")
+    } catch (err) {
+      throw new Error(err)
+    }
   })
 })
